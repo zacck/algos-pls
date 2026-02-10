2486a12cee9f3eff887b8648fe2d4ef3fe111b39
Zacck Osiemo <coderv63@gmail.com>
2026-02-10 07:34:50 +0200

    Add convert to numbers function
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Its an achilles and I should fix it.
 Chapter 1 - Basics 
 
 - [x] check_if_symmetric
-- [ ] convert_to_numbers 
+- [x] convert_to_numbers 
 - [ ] convert_to_letters 
 - [ ] get_intersection
 - [ ] get_union
--- basics/basics.c
+++ basics/basics.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+
+
 
 /*Prototypes*/
 bool check_if_symmetric(char *my_string);
+int *convert_to_numbers_dyn(char *some_string);
 
 /*End Prototypes*/
 
@@ -17,7 +21,48 @@ int main() {
 
 	check_if_symmetric(my_new_string);
 
+
+	char number_test[]  = "a cat";
+
+	int *num_arr = convert_to_numbers_dyn(number_test); 
+
+	if(num_arr) {
+		free(num_arr);
+	}
+
+
 }
+
+int *convert_to_numbers_dyn(char *some_string) {
+	int length = strlen(some_string);
+	
+	printf("The String: %s of length: %d\n", some_string, length);
+
+	int *my_nums = malloc(length);
+	if(!my_nums) 
+		return NULL;
+
+	int character;
+
+	printf("[");
+	for(int i = 0; i < length; i++) {
+		// cast character to integer
+		character = some_string[i];
+		if(character == 32) {
+			my_nums[i] = 0;
+			printf("%d,", my_nums[i]);
+		} else {
+			/* use lower case letters 
+			 * minus position of 'a' plus 1
+			 */
+			my_nums[i] = (character - 97) + 1 ;
+			printf("%d,", my_nums[i]); 
+		}
+	}
+	printf("]");
+	return my_nums;
+}
+
 
 bool check_if_symmetric(char *my_string) {
 	int length = strlen(my_string);
