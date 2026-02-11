60cda9e52255cdaf8e8c27bceb0eb62b852c8793
Zacck Osiemo <coderv63@gmail.com>
2026-02-11 19:34:05 +0200

    Add intersection and convert to letters
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Chapter 1 - Basics
 
 - [x] check_if_symmetric
 - [x] convert_to_numbers 
-- [ ] convert_to_letters 
-- [ ] get_intersection
+- [x] convert_to_letters 
+- [x] get_intersection
 - [ ] get_union
 - [ ] count_characters
 - [ ] is_prime
--- basics/basics.c
+++ basics/basics.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
 
 
 
 /*Prototypes*/
 bool check_if_symmetric(char *my_string);
-int *convert_to_numbers_dyn(char *some_string);
-
+void convert_to_numbers(char *some_string, size_t len);
+void convert_to_letters(int *some_numbers, size_t len);
+void get_intersection(int *first_arr, size_t first, int* second_arr, size_t second);
 /*End Prototypes*/
 
 int main() {
@@ -24,43 +24,111 @@ int main() {
 
 	char number_test[]  = "a cat";
 
-	int *num_arr = convert_to_numbers_dyn(number_test); 
 
-	if(num_arr) {
-		free(num_arr);
+	convert_to_numbers(number_test, strlen(number_test)); 
+
+
+	int some_numbers[] = {1,0,3,1,20};
+
+	size_t some_number_len = sizeof(some_numbers)/sizeof(some_numbers[0]);
+	convert_to_letters(some_numbers, some_number_len);
+
+	int first_numbers[] =  {1, 2, 3 ,4};
+
+	int second_numbers[] = {4, 4, 4, 7};
+
+	get_intersection(first_numbers, 4, second_numbers, 3);
+
+}
+
+void  get_intersection(int *first_arr, size_t first, int* second_arr, size_t second) {
+	size_t longer = 0;  
+	longer = (first > second) ? first : second; 
+
+	int result_arr[longer]; 
+
+	for(int i = 0; i < longer; i++ ) {
+		result_arr[i] = -9999; 
 	}
+
+	int l_index = 0;
+
+	for(int k = 0; k < first; k++) {
+		for(int j = 0; j < second; j++) {
+			if(first_arr[k] == second_arr[j]) {
+				result_arr[l_index] = first_arr[k]; 
+				l_index++;
+			}
+		}
+	}
+
+	int intersection_arr[l_index];
+
+	//dedup into intersection_arr
+	for(int m = 0; m < l_index; m++) {
+		for (int n =  l_index; n >= 0; n--) {
+			if((result_arr[m] == result_arr[n] && n != m) ) {
+				intersection_arr[m] = result_arr[m];
+				l_index--;
+			}
+		}
+	}
+
+	printf("[");
+	for(int p = 0; p < l_index; p++) {
+		printf("%d, ", intersection_arr[p]);
+	}
+	printf("]\n");
+
+	
+
 
 
 }
 
-int *convert_to_numbers_dyn(char *some_string) {
-	int length = strlen(some_string);
-	
-	printf("The String: %s of length: %d\n", some_string, length);
 
-	int *my_nums = malloc(length);
-	if(!my_nums) 
-		return NULL;
+void convert_to_letters(int *some_numbers, size_t len) {
+	printf("the length of numbers is %zu \n",len);
 
-	int character;
+	char character;
 
 	printf("[");
-	for(int i = 0; i < length; i++) {
+	for(int i = 0; i < len; i++) {
 		// cast character to integer
-		character = some_string[i];
-		if(character == 32) {
-			my_nums[i] = 0;
-			printf("%d,", my_nums[i]);
+		character = some_numbers[i];
+		if(character == 0) {
+			printf("%c,", ' ');
 		} else {
 			/* use lower case letters 
 			 * minus position of 'a' plus 1
 			 */
-			my_nums[i] = (character - 97) + 1 ;
-			printf("%d,", my_nums[i]); 
+			printf("%c,", (character + 97) - 1 ); 
 		}
 	}
-	printf("]");
-	return my_nums;
+	printf("] \n");
+
+}
+
+
+void convert_to_numbers(char *some_string, size_t len) {	
+	printf("The String: %s of length: %zu\n", some_string, len);
+
+	char character;
+
+	printf("[");
+	for(int i = 0; i < len; i++) {
+		// cast character to integer
+		character = some_string[i];
+		if(character == 32) {
+			printf("%d,", 0);
+		} else {
+			/* use lower case letters 
+			 * minus position of 'a' plus 1
+			 */
+			printf("%d,", (character - 97) + 1 ); 
+		}
+	}
+	printf("] \n");
 }
 
 
